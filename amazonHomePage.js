549bc5bff5fee816697c21a93f4ca54c764122d4
pcuser399 <turki.mahdi.tn@gmail.com>
2025-08-14 13:04:38 +0100

    amazonHomePage.js

--- amazonHomePage.js
+++ amazonHomePage.js
@@ -1,4 +1,4 @@
-// localStorage.clear()
+localStorage.clear()
 clicks.createNewItem(numberOfAllProducts+numberOfProductInTheMoment)
 votes.initialize()
 display.updateHeadIcons();
@@ -8,3 +8,4 @@ display.allBody(200)
 
 
   
+
